c3089b27ce2fca3b879e3c89e162f0588319470a
Denis Molloy <denis.molloy@gmail.com>
2018-05-03 15:10:51 -0700

    implemented ReactRouter

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# ReduxSimpleStarter
-
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+# ReactReduxRouter Blog
 
 ### Getting Started
 
@@ -12,14 +10,6 @@ Checkout this repo, install dependencies, then start the gulp process with the f
 ```
 > git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
 > cd ReduxSimpleStarter
-> npm install
-> npm start
-```
-
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
 > npm install
 > npm start
 ```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+// Browser Router - interacts with the history library and decides what to do in response to the changes in the URL
+// Route determines what components to show based on the URL
+import { BrowserRouter, Route } from "react-router-dom";
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() {
+    return <div>Hello!</div>;
+  }
+}
+class Goodbye extends React.Component {
+  render() {
+    return <div>Goodbye!</div>;
+  }
+}
+//Route must have a path (usually string) and a component
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
