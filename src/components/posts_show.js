c4fc16adf0cb8a588a0c4f1553af6f720cc8e053
Denis Molloy <denis.molloy@gmail.com>
2018-05-16 15:01:20 -0700

    POSTS_SHOW - added fetchPost action and connected mapStateToProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import fetchPost from '../actions';
 
 class PostsShow extends Component {
+    
+    componentDidMount(){
+        //grabbing the id included in the route that is passed to us by react-router
+        //match is top level property
+        //params - obj that contains all the wildcard tokens that exist inside the url
+        const {id} = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
+    // posts[this.props.match.params.id]//the post we want to show, but we don't want to pull in all the posts (wasteful)
     render () {
         return (
             <div>
@@ -10,4 +22,17 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow;
+
+//posts is currently the full object of posts from the state
+//first arg == application state
+//second arg == conventionally name ownProps - this.props(in the render method) === ownProps
+//common to modularize mapStateToProps
+function mapStateToProps({ posts }, ownProps){
+    //the post will only receive the specific post we care about
+    return { post: posts[ownProps.match.params.id] };
+}
+
+// CONNECT() TAKES TWO PARAMETERS
+//first - a function that describes what part of the redux store we want to use on this component
+//second - what actionCreators we want to use in the component to send data to the redux store
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
