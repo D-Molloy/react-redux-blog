5754a18a5ce1448c25a300e0f3a6af3497f474d7
Denis Molloy <denis.molloy@gmail.com>
2018-05-16 14:06:48 -0700

    added fetchPost action creator and added FETCHPOST reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
 const ROOT_URL = "http://reduxblog.herokuapp.com/api/"
 const API_KEY = "?key=98126hat"
 
@@ -16,13 +17,21 @@ export function fetchPosts(){
 }
 
 //axios returns a promise, so we can add a .then and call our callback (the redirect in posts_new onSubmit)
-export function createPost(values, callback) {
+export function createPost(values, cb) {
 
     const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
-    .then(()=> callback());
+    .then(()=> cb());
 
     return {
         type: CREATE_POST,
+        payload: request
+    }
+}
+
+export function fetchPost(id){
+    const request = axios.post(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    return {
+        type: FETCH_POST,
         payload: request
     }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,24 @@
-import { FETCH_POSTS, CREATE_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash'
 
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            //ES5 method
+            // // action.payload.data == response data
+            // const post = action.payload.data;
+            // // use the existing state and spread it into the new object
+            // const newState =  {...state };
+            // // take the newState object, add on the additional post.id property, and set it equal to post
+            // newState[post.id] = post;
+            // return newState;
+            // ES6 method below identical to ES5 version 
+            // [action.payload.data.id]: action.payload.data === key interpolation
+            // we are making a new key on the returned object of whatever action.payload.data.id is, and assigning it the value of the payload
+            return {...state, [action.payload.data.id]: action.payload.data }
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
-        // case CREATE_POST:
-        //     return
         default:
             return state;
     }
